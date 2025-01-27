e5dbe3f9a9fa46db8e4fbb74a81e1ddcb9174aaa
ItzLucas1324 <addaboy13@gmail.com>
2025-01-26 21:17:15 -0500

    Re made MusicManager script to fix over lapping music track problem

--- 3001_AI_As1/Assets/_MyAssets/_Scripts/MusicManager.cs
+++ 3001_AI_As1/Assets/_MyAssets/_Scripts/MusicManager.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
     [SerializeField] AudioClip introMusic;
     [SerializeField] AudioClip mainMusic;
-
+    
     private AudioSource introAudioSource;
     private AudioSource mainAudioSource;
-
     private static MusicManager instance;
+
     private void Awake()
     {
         if (instance == null)
@@ -23,25 +21,62 @@ public class MusicManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     private void Start()
     {
-        introAudioSource = gameObject.AddComponent<AudioSource>();
-        mainAudioSource = gameObject.AddComponent<AudioSource>();
-
+        // Create separate GameObjects for each audio source to ensure clean separation
+        GameObject introAudioObj = new GameObject("IntroAudio");
+        GameObject mainAudioObj = new GameObject("MainAudio");
+        
+        introAudioObj.transform.parent = transform;
+        mainAudioObj.transform.parent = transform;
+        
+        introAudioSource = introAudioObj.AddComponent<AudioSource>();
+        mainAudioSource = mainAudioObj.AddComponent<AudioSource>();
+        
+        // Configure audio sources
         introAudioSource.volume = 0.5f;
         mainAudioSource.volume = 0.5f;
         introAudioSource.loop = false;
         mainAudioSource.loop = true;
-
+        
         PlayMusic();
     }
+
     private void PlayMusic()
     {
         introAudioSource.clip = introMusic;
-        introAudioSource.Play();
-
-        double introEndTime = AudioSettings.dspTime + introMusic.length;
         mainAudioSource.clip = mainMusic;
-        mainAudioSource.PlayScheduled(introEndTime);
+        
+        // Start intro music
+        introAudioSource.Play();
+        
+        // Start coroutine to monitor intro music completion
+        StartCoroutine(MonitorIntroMusic());
+    }
+
+    private System.Collections.IEnumerator MonitorIntroMusic()
+    {
+        
+        // Wait until near the end of the intro music
+        while (introAudioSource.time < introMusic.length - 0.1f)
+        {
+            yield return null;
+        }
+        
+        // Start main music
+        mainAudioSource.Play();
+        
+        // Optional fade out for intro music
+        float fadeTime = 0.1f;
+        float startVolume = introAudioSource.volume;
+        
+        while (introAudioSource.volume > 0)
+        {
+            introAudioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+        
+        introAudioSource.Stop();
     }
 }
