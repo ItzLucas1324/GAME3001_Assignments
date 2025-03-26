dd67ce600d8b3aafd79b507067034474946c2e8e
Lucas Adda <addaboy13@gmail.com>
2025-03-25 21:57:41 -0400

    Basic movement animation complete

--- 3001_AI_As3/Assets/_MyAssets/_Scripts/PlayerScript.cs
+++ 3001_AI_As3/Assets/_MyAssets/_Scripts/PlayerScript.cs
@@ -8,11 +8,13 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rb;
     private float lastHorizontalInput = 0;
     private float lastVerticalInput = 0;
+    private Animator an;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
+        an = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -24,18 +26,29 @@ public class PlayerScript : MonoBehaviour
         {
             lastHorizontalInput = moveX;
             lastVerticalInput = 0;
+
+            an.SetBool("isRunning", true);
+            an.SetFloat("moveDirection", moveX);
+            an.SetBool("isMovingVertically", false);
         }
 
         if (moveY != 0)
         {
             lastVerticalInput = moveY;
             lastHorizontalInput = 0;
+            an.SetBool("isMovingVertically", true);
+            an.SetFloat("moveDirectionY", moveY);
+            an.SetBool("isRunning", true);
         }
 
-        if (moveX == 0 && moveY ==0)
+        if (moveX == 0 && moveY == 0)
         {
             lastHorizontalInput = 0;
             lastVerticalInput = 0;
+            an.SetBool("isRunning", false);
+            an.SetFloat("moveDirection", moveX);
+            an.SetBool("isMovingVertically", false);
+            an.SetFloat("moveDirectionY", moveY);
         }
 
         Vector2 moveDirection = new Vector2(lastHorizontalInput, lastVerticalInput).normalized;
